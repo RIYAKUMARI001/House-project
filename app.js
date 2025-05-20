8ea282aab5a35ec58547750e21292deed45611b0
Riya kumari <riya010206@gmail.com>
2025-05-20 11:30:53 +0530

    Add ejs file for frontend

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/test";
 
@@ -14,25 +15,61 @@ async function main() {
 app.get ("/", (req, res) => {
     res.send("Hello World");
 })
-
+//Index Route
 app.get("/listings", async (req, res) => {
     const allListing= await Listing.find({});
-    res.render("index.ejs", {listings: allListing});
+    res.render("listings/index.ejs", {listings: allListing});
     });
-// app.get("/listings", async (req, res) => {
-//     let sampleListing = new Listing({
-//         title: "test",
-//         description: "test",
-//         price: 100,
-//         image: "https://images.unsplash.com/photo-1746950862855-ccd90286a57c?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8RnpvM3p1T0hONnd8fGVufDB8fHx8fA==",
-//         location: "test",
-//         country: "test"
+
+//New Route
+app.get("/listings/new", (req, res) => {
+    res.render("listings/new.ejs");
+    });
+
+//Show Route
+app.get("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id); 
+    res.render("listings/show.ejs", {listing});
+    });
+    app.set("view engine", "ejs");
+    app.set("views", path.join(__dirname, "views"));
+    app.use(express.urlencoded({extended: true}));
+    app.use(methodOverride("_method"));
+
+//New Route
+// app.get("/listings/new", (req, res) => {
+//     res.render("listings/new.ejs");
 //     });
 
-//    await sampleListing.save();
-//    console.log("sample listing saved");
-//    res.send("Listing saved");
-// });
+//Create Route App
+app.post("/listings", async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect(`/listings/${newListing._id}`);
+})
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id); 
+    res.render("listings/edit.ejs", {listing});
+})
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+   await Listing.findByIdAndUpdate(id,{...req.body.listing})
+   res.redirect(`/listings/${id}`);
+})
+
+//DELETE Route
+app.delete("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+})
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
+
