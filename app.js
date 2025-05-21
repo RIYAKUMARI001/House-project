b67a7b1c9462a7beb96c60e7889c55337feb207c
Riya kumari <riya010206@gmail.com>
2025-05-21 23:52:11 +0530

    add frontend ejs file

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/test";
 
@@ -36,6 +37,8 @@ app.get("/listings/:id", async (req, res) => {
     app.set("views", path.join(__dirname, "views"));
     app.use(express.urlencoded({extended: true}));
     app.use(methodOverride("_method"));
+    app.engine("ejs", ejsMate);
+    app.use(express.static(path.join(__dirname, "public")));
 
 //New Route
 // app.get("/listings/new", (req, res) => {
